6929d136082c5c2cc7ef7db883fd12124da0de63
ABI2525 <727721euec004@skcet.ac.in>
2023-03-29 11:08:19 +0530

    msg

---
+++ src/ClassProps.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClassProps extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is World Class Component</h1>
+      </div>
+    )
+  }
+}
